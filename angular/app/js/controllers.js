514e64ba489436c4a4c069ff4de96c18fd71b8e0
nvernooy <vernooy.n@gmail.com>
2015-01-11 09:44:23 -0800

    Angular structure displaying JSON response

---
+++ angular/app/js/controllers.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/* Controllers */
+
+var costItems = angular.module('costItems', []);
+
+costItems.controller('ItemListCtrl', ['$scope', '$http', function($scope, $http) {
+  //$http.get('cost_items/cost_items.json').success(function(data) {
+    $http.get('http://0.0.0.0:6543').success(function(data) {
+    $scope.projects = data;
+  });
+}]);
