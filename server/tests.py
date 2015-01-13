caa883f436e66039dda8598f90468c150a46f368
nvernooy <vernooy.n@gmail.com>
2015-01-14 00:08:43 +0200

    Server working. Client Working. ZODB with persistent objects wokring. CORS issue fixed. Issue: JSON response returns empty list when reading file/db.

--- server/tests.py
+++ server/tests.py
@@ -1,15 +1,48 @@
-"""
-Unit test for this package
-"""
-
 import unittest
 
+from pyramid import testing
 
-class FunctionalTests(unittest.TestCase):
-    """ There is only one test case so far,
-    whether the response is a json data structure"""
 
-    def json_data(self):
-        """Test if the response is a JSON object."""
+class ServerViewTests(unittest.TestCase):
+    """Test server response."""
+
+    def setUp(self):
+        """Set up testing configuration"""
+        self.config = testing.setUp()
+
+    def tearDown(self):
+        """Tear down setup"""
+        testing.tearDown()
+
+    def test_list_not_empty(self):
+        """Test data is in the list"""
+        import views
+
+        request = testing.DummyRequest()
+        response = views.formatdata(request)
+        self.assertNotEqual(0, len(response))
+
+    def test_data_response(self):
+        """Test data sent by JSON"""
+        import views
+
+        request = testing.DummyRequest()
+        response = views.formatdata(request)
+        self.assertEqual("A", response[0]["Name"])
+
+
+class ServerFunctionalTests(unittest.TestCase):
+    """Functional tests here"""
+
+    def setUp(self):
+        """Setup enviroment"""
+        from server import main
+        app = main({})
+        from webtest import TestApp
+
+        self.testapp = TestApp(app)
+
+    def test_object_json(self):
+        """Test response is JSON"""
         res = self.testapp.get('/', status=200)
         self.assertEqual(res.content_type, 'application/json')
--- server/views.py
+++ server/views.py
@@ -7,120 +7,171 @@ of the cost estimate items
 
 from pyramid.view import view_config
 from models import Project, BudgetGroup, BudgetItem
+import ZODB
+import ZODB.FileStorage
+import transaction
+from BTrees.OOBTree import OOBTree
 
 
-class DataViews:
-    """Class acts as a mediator between data and the output thereof.
+@view_config(route_name='Data', renderer='json')
+def formatdata(request):
+    """The method gets a list of the data structures and transforms
+    it into a format usable by JSON.
 
-    Method convertdata() gets a list and transforms it into a list
-    of dictionaries for use by the JSON renderer. """
+    The list comes from databasedata() and is formatted into a list of
+    dictionaries, the result is returned."""
 
-    def __init__(self, request):
-        """init method to make the class into a module"""
-        self.request = request
+    # Do the CORS thingie
+    response = request.response
 
-    @view_config(route_name='Data', renderer='json')
-    def formatdata(self):
-        """The method gets a list of the data structures and transforms
-        it into a format usable by JSON.
+    response.headers["Access-Control-Allow-Origin"] = "*"
+    response.headers["Content-Type"] = "application/json"
+    response.headers["Access-Control-Allow-Headers"] =     \
+                    "Origin, X-Requested-With, Content-Type, Accept"
+    response.headers["Access-Control-Allow-Methods"] = "POST, GET, PUT, DELETE"
 
-        The list comes from filedata and is formatted into a list of
-        dictionaries, the result is returned."""
+    # Get the list from the filedata() method and declare an empty list.
+    preformatlist = databasedata()
+    #preformatlist = filedata()
+    JSONlist = []
 
-        # Get the list from the filedata() method and declare an empty list.
-        projectlist = self.filedata()
-        JSONlist = []
+    # Iterate through the list and convert the data into dictionaries.
+    for project in preformatlist:
+        budgetgrouplist = []
 
-        # Iterate through the list and convert the data into dictionaries.
-        for project in projectlist:
-            budgetgrouplist = []
+        for budgetgroup in project.GroupSet:
+            budgetitemlist = []
 
-            for budgetgroup in project.GroupSet:
-                budgetitemlist = []
+            for budgetitem in budgetgroup.ItemSet:
+                budgetitemlist.insert(len(budgetitemlist),
+                    {"Name": budgetitem.Name,
+                    "Description": budgetitem.Description,
+                    "Quantity": budgetitem.Quantity,
+                    "Rate": budgetitem.Rate})
 
-                for budgetitem in budgetgroup.ItemSet:
-                    budgetitemlist.insert(len(budgetitemlist),
-                        {"Name": budgetitem.Name,
-                        "Description": budgetitem.Description,
-                        "Quantity": budgetitem.Quantity,
-                        "Rate": budgetitem.Rate})
+            # Build each list and then add it to the dictionary
+            budgetgrouplist.insert(len(budgetitemlist),
+                {"Name": budgetgroup.Name,
+                "Description": budgetgroup.Description,
+                "BudgetItems": budgetitemlist})
 
-                # Build each list and then add it to the dictionary
-                budgetgrouplist.insert(len(budgetitemlist),
-                    {"Name": budgetgroup.Name,
-                    "Description": budgetgroup.Description,
-                    "BudgetItems": budgetitemlist})
+        JSONlist.insert(len(budgetitemlist),
+            {"Name": project.Name,
+            "Description": project.Description,
+            "BudgetGroup": budgetgrouplist})
 
-            JSONlist.insert(len(budgetitemlist),
-                {"Name": project.Name,
-                "Description": project.Description,
-                "BudgetGroup": budgetgrouplist})
+    # Return the finished list of projects, groups, and items.
+    return JSONlist
 
-        # Return the finished list of projects, groups, and items.
-        return JSONlist
-
-    def filedata(self):
-        """
-        Add data to the database using a file on the system.
-
-        The file is preformatted to enable the method to parse
-        and build the relevant data structures.
-        """
-
-        projectlist = []
-        try:
-            # Open the data file and read the first line.
-            datafile = open("simpledata.txt", "r")
-            line = datafile.next().rstrip()
-
-            # Iterate until the End Of File is reached.
-            while line != "EOF":
-
-                # "=" indicates the following is a Project structure
-                if line == "=":
-                    # Read the data and initialise the Project.
-                    name = datafile.next().rstrip()
-                    desc = datafile.next().rstrip()
-                    project = Project(name, desc)
-
-                    line = datafile.next().rstrip()
-                    while line != "=" and line != "EOF":
-                        # "+" indicates a BudgetGroup
-                        if line == "+":
-                            gname = datafile.next().rstrip()
-                            gdesc = datafile.next().rstrip()
-                            budgetgroup = BudgetGroup(gname, gdesc)
-                            line = datafile.next().rstrip()
-
-                            while line != "+" and line != "=":
-                                # "*" indicates a BudgetItem
-                                if line == "*":
-                                    iname = datafile.next().rstrip()
-                                    idesc = datafile.next().rstrip()
-                                    iquantity = float(datafile.next().rstrip())
-                                    irate = float(datafile.next().rstrip())
-
-                                    budgetitem = BudgetItem(iname, idesc,
-                                                    iquantity, irate)
-
-                                    # Add the items to the BudgetGroup list.
-                                    budgetgroup.add(budgetitem)
-                                    line = datafile.next().rstrip()
-
-                                # If the EOF is reached stop the iteration
-                                if line == "EOF":
-                                    break
-                            # Add the BudgetGroup list to the Project list
-                            project.add(budgetgroup)
-                    projectlist.insert(len(projectlist), project)
-
-            datafile.close()
-        except:
-            pass
-
-        # Return the complete list of data structures
-        return projectlist
+    #return [{"Name":"A", "Description":"desc"}]
 
 
-"""if __name__ == "__main__":
-    print DataViews(None).formatdata()"""
+def filedata():
+    """
+    Add data to the database using a file on the system.
+
+    The file is preformatted to enable the method to parse
+    and build the relevant data structures.
+    """
+
+    projectlist = []
+    try:
+        # Open the data file and read the first line.
+        datafile = open("simpledata.txt", "r")
+        line = datafile.next().rstrip()
+
+        # Iterate until the End Of File is reached.
+        while line != "EOF":
+
+            # "=" indicates the following is a Project structure
+            if line == "=":
+                # Read the data and initialise the Project.
+                name = datafile.next().rstrip()
+                desc = datafile.next().rstrip()
+                project = Project(name, desc)
+
+                line = datafile.next().rstrip()
+                while line != "=" and line != "EOF":
+                    # "+" indicates a BudgetGroup
+                    if line == "+":
+                        gname = datafile.next().rstrip()
+                        gdesc = datafile.next().rstrip()
+                        budgetgroup = BudgetGroup(gname, gdesc)
+                        line = datafile.next().rstrip()
+
+                        while line != "+" and line != "=":
+                            # "*" indicates a BudgetItem
+                            if line == "*":
+                                iname = datafile.next().rstrip()
+                                idesc = datafile.next().rstrip()
+                                iquantity = float(datafile.next().rstrip())
+                                irate = float(datafile.next().rstrip())
+
+                                budgetitem = BudgetItem(iname, idesc,
+                                                iquantity, irate)
+
+                                # Add the items to the BudgetGroup list.
+                                budgetgroup.add(budgetitem)
+                                line = datafile.next().rstrip()
+
+                            # If the EOF is reached stop the iteration
+                            if line == "EOF":
+                                break
+                        # Add the BudgetGroup list to the Project list
+                        project.add(budgetgroup)
+
+                projectlist.insert(len(projectlist), project)
+        datafile.close()
+    except:
+        pass
+
+    # Return the complete list of data structures
+    return projectlist
+
+
+def databasedata():
+    """The method creates a connection with ZODB and manages the project
+    data that is in it.
+
+    It adds project data to the database if it is not there, otherwise
+    it prints out all the data in the database.
+    """
+
+    # Create the database connection.
+    storage = ZODB.FileStorage.FileStorage('ProjectData.fs')
+    db = ZODB.DB(storage)
+    connection = db.open()
+    projectDB = connection.root()
+
+    # If the projects table does not exist in the database, create it.
+    if len(projectDB) == 0:
+        projectDB.projects = OOBTree()
+
+        # Run the addData method that returns a finished Project
+        projectdatalist = filedata()
+
+        # Add the Project list to the database,
+        # using the Project ID as the key.
+        for project in projectdatalist:
+            projectDB.projects[project.ID] = project
+            # Commit the change to the database
+            transaction.commit()
+
+    # Get a list of the items in the database
+    datalist = []
+    for key in projectDB.projects.keys():
+        datalist.insert(len(datalist), projectDB.projects[key])
+
+    # Close the database
+    transaction.commit()
+    connection.close()
+    db.close()
+    storage.close()
+
+    # Return the list
+    return datalist
+
+#if __name__ == "__main__":
+    #tmplist = formatdata(None)
+    #for project in tmplist:
+        #print project
